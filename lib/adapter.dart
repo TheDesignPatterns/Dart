2911d1af2ddcfe3296c2700d2ddd53acfff37d3c
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-21 15:41:56 +0000

    .

--- lib/adapter.dart
+++ lib/adapter.dart
@@ -1,29 +1,28 @@
-import 'dart:typed_data';
-
 /// A target interface that Client uses
 abstract class AbstractTarget {
-	String request(String input);
+  String request(String input);
 }
 
 /// An adapter that adapts to target interface
 ///
 /// Input/Output in form of String is adapted to/from Runes
 class Adapter implements AbstractTarget {
-	final AbstractAdaptee _adaptee;
+  final AbstractAdaptee _adaptee;
 
-	Adapter(this._adaptee);
+  Adapter(this._adaptee);
 
-	@override
-	String request(String input) => _adaptee.specificRequest(input.runes).string;
+  @override
+  String request(String input) => _adaptee.specificRequest(input.runes).string;
 }
 
 /// An interface that needs adapting
 abstract class AbstractAdaptee {
-	Runes specificRequest(Runes input);
+  Runes specificRequest(Runes input);
 }
 
 /// An Adaptee that puts Runes in reverse order
 class Adaptee implements AbstractAdaptee {
-	@override
-	Runes specificRequest(Runes input) => String.fromCharCodes(input.toList().reversed).runes;
+  @override
+  Runes specificRequest(Runes input) =>
+      String.fromCharCodes(input.toList().reversed).runes;
 }
