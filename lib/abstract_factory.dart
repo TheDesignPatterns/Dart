ac9cc879f9e36509662997c10f9b6e55757e75f7
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-27 07:49:27 +0100

    .

--- lib/abstract_factory.dart
+++ lib/abstract_factory.dart
@@ -1,46 +1,46 @@
-abstract class AbstractProductA {
-	void operation();
-}
-
-class ProductA1 implements AbstractProductA {
-	@override
-	void operation() => print(this);
-}
-
-class ProductA2 implements AbstractProductA {
-	@override
-	void operation() => print(this);
-}
-
-abstract class AbstractProductB {
-	void operation();
-}
-
-class ProductB1 implements AbstractProductB {
-	@override
-	void operation() => print(this);
-}
-
-class ProductB2 implements AbstractProductB {
-	@override
-	void operation() => print(this);
-}
-
 abstract class AbstractFactory {
-	AbstractProductA createProductA();
-	AbstractProductB createProductB();
+  AbstractProductA createProductA();
+  AbstractProductB createProductB();
 }
 
 class Factory1 implements AbstractFactory {
-	@override
-	AbstractProductA createProductA() => ProductA1();
-	@override
-	AbstractProductB createProductB() => ProductB1();
+  @override
+  AbstractProductA createProductA() => ProductA1();
+  @override
+  AbstractProductB createProductB() => ProductB1();
 }
 
 class Factory2 implements AbstractFactory {
-	@override
-	AbstractProductA createProductA() => ProductA2();
-	@override
-	AbstractProductB createProductB() => ProductB2();
+  @override
+  AbstractProductA createProductA() => ProductA2();
+  @override
+  AbstractProductB createProductB() => ProductB2();
+}
+
+abstract class AbstractProductA {
+  void operation();
+}
+
+class ProductA1 implements AbstractProductA {
+  @override
+  void operation() => print(this);
+}
+
+class ProductA2 implements AbstractProductA {
+  @override
+  void operation() => print(this);
+}
+
+abstract class AbstractProductB {
+  void operation();
+}
+
+class ProductB1 implements AbstractProductB {
+  @override
+  void operation() => print(this);
+}
+
+class ProductB2 implements AbstractProductB {
+  @override
+  void operation() => print(this);
 }
