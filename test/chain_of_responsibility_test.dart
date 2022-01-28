7b670da53b23776088701b9b94874a8362ce7350
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-28 11:56:48 +0100

    .

--- test/chain_of_responsibility_test.dart
+++ test/chain_of_responsibility_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the Design Patterns project authors.
+// All rights reserved. Use of this source code is governed by a
+// MIT-style license that can be found in the LICENSE file.
+
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
