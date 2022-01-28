5760796e949cde0df8c0927a43d513ea62af5aac
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-28 09:28:13 +0100

    .

--- lib/decorator.dart
+++ lib/decorator.dart
@@ -1,3 +1,17 @@
+/// Decorates given element as shown in the example below
+///
+/// ```dart
+/// void main() {
+///   var foo = Foo();
+///   var fooBar = Decorator(foo, 'Bar');
+///   var fooBarBaz = Decorator(fooBar, 'Baz');
+///
+///   print(foo.operation()); // Prints "Foo".
+///   print(fooBar.operation()); // Prints "FooBar".
+///   print(fooBarBaz.operation()); // Prints "FooBarBaz".
+/// }
+/// ```
+
 abstract class AbstractComponent<R> {
   R operation();
 }
