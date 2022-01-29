0111b698f80814b76035fc387cf41e4b0ae41846
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-29 18:38:49 +0100

    .

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,11 +1,11 @@
 /// ```dart
-/// var tokens = [Token(Type.A, 'xx'), Token(Type.B, 'yy'), Token(Type.A, 'zz')];
+/// var tokens = [Token(Type.A, 'xxx'), Token(Type.B, 'yyy'), Token(Type.A, 'zzz')];
 ///
 /// var foo = Director(FooBuilder()).construct(tokens);
 /// var bar = Director(BarBuilder()).construct(tokens);
 ///
-/// print(foo); // Prints "[<A>xx</A>, <B>yy</B>, <A>zz</A>]".
-/// print(bar); // Prints "A=[xx, zz] B=[yy]".
+/// print(foo); // Prints "[<A>xxx</A>, <B>yyy</B>, <A>zzz</A>]".
+/// print(bar); // Prints "A=[xxx, zzz] B=[yyy]".
 /// ```
 
 enum Type { A, B }
