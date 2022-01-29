13cd6fd4ea0a214cfbf04bb6dda20e88b8c20da8
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-01-29 07:11:46 +0100

    .

--- lib/proxy.dart
+++ lib/proxy.dart
@@ -2,11 +2,11 @@
 /// var foo = Foo();
 /// var fooProxy = Proxy(foo, forRejecting: [Request.baz]);
 ///
-/// print(foo.handle(Request.bar)); // Prints "Status.handled"
-/// print(foo.handle(Request.baz)); // Prints "Status.handled"
+/// print(foo.handle(Request.bar)); // Prints "Status.handled".
+/// print(foo.handle(Request.baz)); // Prints "Status.handled".
 ///
-/// print(fooProxy.handle(Request.bar)); // Prints "Status.handled"
-/// print(fooProxy.handle(Request.baz)); // Prints "Status.rejected"
+/// print(fooProxy.handle(Request.bar)); // Prints "Status.handled".
+/// print(fooProxy.handle(Request.baz)); // Prints "Status.rejected".
 /// ```
 
 enum Request { bar, baz }
