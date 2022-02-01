050704543cbaa2a9821522b473c2741c21cecdd3
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-02-01 08:11:21 +0100

    .

--- lib/iterator.dart
+++ lib/iterator.dart
@@ -1,3 +1,12 @@
+/// ```dart
+/// var aggregator = Aggregator(['foo', 'bar', 'baz']);
+/// var iterator = aggregator.createIterator();
+///
+/// while (iterator.hasNext()) {
+///   print(iterator.next()); // Prints: "foo" "bar" "baz"
+/// }
+/// ```
+
 abstract class AbstractIterator<T> {
   T? next();
   bool hasNext();
