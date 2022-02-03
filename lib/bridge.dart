70510ce23df0ecad59b197026f005d528d9ad872
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-02-03 12:18:37 +0100

    .

--- lib/bridge.dart
+++ lib/bridge.dart
@@ -1,6 +1,6 @@
 /// ```dart
-/// var fooBar = Foo(' is implemented by ', Bar());
-/// var fooBaz = Foo(' |=======|=======| ', Baz());
+/// var fooBar = Foo('is implemented by', Bar());
+/// var fooBaz = Foo('|=======|=======|', Baz());
 ///
 /// print(fooBar.operation()); // Prints "Foo is implemented by bar".
 /// print(fooBaz.operation()); // Prints "Foo |=======|=======| BAZ".
