587399024869e2026ba83b3edc3358bb69235937
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-02-05 15:28:37 +0100

    .

---
+++ test/interpreter_test.dart
@@ -0,0 +1,35 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:dp/interpreter.dart';
+import 'interpreter_test.mocks.dart';
+
+@GenerateMocks([AbstractExpression])
+void main() {
+  late AbstractExpression leftExpressionMock;
+  late AbstractExpression rightExpressionMock;
+  final context = {'a': 20, 'b': 10};
+
+  setUp(() {
+      leftExpressionMock = MockAbstractExpression();
+      rightExpressionMock = MockAbstractExpression();
+      when(leftExpressionMock.interpret(context)).thenReturn(20);
+      when(rightExpressionMock.interpret(context)).thenReturn(10);
+  });
+
+  test('Var(iable) returns associated value', () {
+    final variable = Var('a');
+    expect(variable.interpret(context), 20);
+  });
+
+  test('Plus adds two expressions', () {
+    final plus = Plus(leftExpressionMock, rightExpressionMock);
+    expect(plus.interpret(context), 30);
+  });
+
+  test('Minus subtracts two expressions', () {
+    final minus = Minus(leftExpressionMock, rightExpressionMock);
+    expect(minus.interpret(context), 10);
+  });
+}
