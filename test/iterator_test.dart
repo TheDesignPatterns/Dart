bfc0dc60bb05acbbb0b401c217e26cd2ecb99dff
mehoffer <73532423+mehoffer@users.noreply.github.com>
2022-02-07 11:16:09 +0100

    .

--- test/iterator_test.dart
+++ test/iterator_test.dart
@@ -2,6 +2,35 @@ import 'package:test/test.dart';
 import 'package:dp/iterator.dart';
 
 void main() {
-  test('', () {
+  group('Iterator.hasNext', () {
+    test('returns false for empty collection', () {
+      final iterator = Iterator([]);
+      expect(iterator.hasNext(), isFalse);
+    });
+
+    test('returns true for non-empty collection', () {
+      final iterator = Iterator(['foo']);
+      expect(iterator.hasNext(), isTrue);
+    });
+
+    test('returns false unless next element', () {
+      final iterator = Iterator(['foo']);
+      iterator.next();
+      expect(iterator.hasNext(), isFalse);
+    });
+  });
+
+  group('Iterator.next', () {
+    test('returns value if next element', () {
+      final iterator = Iterator(['foo', 'bar']);
+      expect(iterator.next(), 'foo');
+      expect(iterator.next(), 'bar');
+    });
+
+    test('returns null unless next element', () {
+      final iterator = Iterator(['foo']);
+      expect(iterator.next(), 'foo');
+      expect(iterator.next(), null);
+    });
   });
 }
