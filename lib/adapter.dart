8851a1f781b43133bc85464502546b4308028e4e
mehoffer <mehoffer@protonmail.com>
2022-02-10 17:47:08 +0100

    removed comments for consistency

--- lib/adapter.dart
+++ lib/adapter.dart
@@ -14,12 +14,10 @@
 /// print(adapter.request('oof')); // prints foo
 /// ```
 
-/// A target interface that client uses
 abstract class AbstractTarget {
   String request(String input);
 }
 
-/// An adapter that adapts String to/from Runes (integer Unicode points)
 class Adapter implements AbstractTarget {
   final AbstractAdaptee _adaptee;
 
@@ -29,12 +27,10 @@ class Adapter implements AbstractTarget {
   String request(String input) => _adaptee.specificRequest(input.runes).string;
 }
 
-/// An interface that needs adapting
 abstract class AbstractAdaptee {
   Runes specificRequest(Runes input);
 }
 
-/// An Adaptee that puts Runes in reverse order
 class Adaptee implements AbstractAdaptee {
   @override
   Runes specificRequest(Runes input) =>
